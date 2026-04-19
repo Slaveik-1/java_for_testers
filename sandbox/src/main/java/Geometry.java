21338bfef81af929e3327d0ac17b37413c089e6d
pupsovich <slawa2013@gmail.com>
2026-04-19 13:35:41 +0400

    Добавил значения 1 и 2 лекции

---
+++ sandbox/src/main/java/Geometry.java
@@ -0,0 +1,8 @@
+public class Geometry {
+    public static void main(String[] args) {
+        //Лекция 1.3 Переменные и значения
+        var integer_1 = 5d;
+        System.out.println("Площадь квадрата " + integer_1 + "=" +(integer_1*integer_1));
+        System.out.println(Math.pow(integer_1,integer_1));
+    }
+}
--- sandbox/src/main/java/Hello.java
+++ sandbox/src/main/java/Hello.java
@@ -3,5 +3,12 @@ public class Hello {
 
     public static void main(String[] args) {
         System.out.println(Hello);
+
+        //Лекция 1.2 мат. функции
+        System.out.println(1d/2d + " =)");
+        //Конкатенация
+        System.out.println("Hello" + ", " + "world"+"!");
+        //Интересно...
+        System.out.println("2+2="+2+2);
     }
 }
