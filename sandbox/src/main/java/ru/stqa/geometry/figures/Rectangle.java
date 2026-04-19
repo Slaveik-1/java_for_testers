d128cb317349ea814001e0835fd5084d50872420
pupsovich <slawa2013@gmail.com>
2026-04-19 20:32:39 +0400

    Лекция 1.6
    Структуризация кода

--- sandbox/src/main/java/ru/stqa/geometry/figures/Rectangle.java
+++ sandbox/src/main/java/ru/stqa/geometry/figures/Rectangle.java
@@ -3,7 +3,8 @@ package ru.stqa.geometry.figures;
 public class Rectangle {
 
     public static void printRectangleArea(double a, double b) {
-        System.out.println("Площадь прямоугольника со сторонами " + a + " и "+ b + " = " + printRectangleeArea(a, b));
+        var text =String.format("Площадь прямоугольника со сторонами со сторонами %f и %f = %f", a,b,printRectangleeArea(a,b));
+        System.out.println(text);
     }
 
     private static double printRectangleeArea(double a, double b) {
--- sandbox/src/main/java/ru/stqa/geometry/figures/Square.java
+++ sandbox/src/main/java/ru/stqa/geometry/figures/Square.java
@@ -4,6 +4,9 @@ public class Square {
 
     public static void printSquareArea(double side){
         System.out.println("Площадь квадрата со стороной " + side + " = " + squareArea(side));
+        System.out.println(String.format(
+                "Площадь квадрата со стороной %f = %f", side, squareArea(side)
+        ));
     }
 
     private static double squareArea(double side) {
