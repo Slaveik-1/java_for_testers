c6772882855c1ff61c80066d18c5a48f8e7b24b5
pupsovich <slawa2013@gmail.com>
2026-05-24 23:22:31 +0400

    Лекция 5.1

--- addresbook_web_tests/src/test/java/tests/Contact/ContactCreationTests.java
+++ addresbook_web_tests/src/test/java/tests/Contact/ContactCreationTests.java
@@ -30,14 +30,16 @@ public class ContactCreationTests extends TestBase {
 
     @Test
     public void canCreateContactFIO(){
-        var emptyContact = new ContactData();
-        var contactFIO = emptyContact.withFIO("Пипяу","Пипяуович","Пипяуов");
-        app.contactHelper().createContact(contactFIO);
+        var emptyContact = new ContactData()
+                .withFirstname("Пипяу")
+                .withLastname("Пипяуов")
+                .withPhoto("src/test/resources/images/avatar.png");
+        app.contactHelper().createContact(emptyContact);
     }
 
     @Test
     public void canCreateFull(){
-        app.contactHelper().createContact(new ContactData("", "Пипяу","Пипяуович","Пипяуов","Pipyau","Title","OOO Pipyau",
+        app.contactHelper().createContact(new ContactData("", "Пипяу","Пипяуович","Пипяуов","Pipyau", "", "Title","OOO Pipyau",
                 "The Pentagon, Washington, DC 20301-0003","dom","123","work","e1",
                 "e2","e3","home"));
     }
--- sandbox/src/main/java/Hello.java
+++ sandbox/src/main/java/Hello.java
@@ -5,27 +5,28 @@ public class Hello {
 
     public static void main(String[] args) {
 
-        try {
-            var x = 1;
-            var y = 1;
-            if(y==0){
-                System.out.println("Не едели на ноль пж");
-            }else {
-                var z = divide(x, y);
-                //System.out.println(z);
-            }
-        }catch (ArithmeticException e){
-            System.out.println("Не едели на ноль пж");
-        }
-        System.out.println(Hello);
+//        try {
+//            var x = 1;
+//            var y = 1;
+//            if(y==0){
+//                System.out.println("Не едели на ноль пж");
+//            }else {
+//                var z = divide(x, y);
+//                //System.out.println(z);
+//            }
+//        }catch (ArithmeticException e){
+//            System.out.println("Не едели на ноль пж");
+//        }
+//        System.out.println(Hello);
 
-//        var configFile = new File("sandbox/build.gradle");
-//        System.out.println(configFile.getAbsolutePath());
-//        System.out.println(configFile.exists());
+        var configFile = new File("sandbox/build.gradle");
+        System.out.println(configFile.getAbsolutePath());
+        System.out.println(configFile.exists());
+        System.out.println(new File("").getAbsolutePath());
     }
 
-    private static int divide(int x, int y) {
-        var z = x / y;
-        return z;
-    }
+//    private static int divide(int x, int y) {
+//        var z = x / y;
+//        return z;
+//    }
 }
