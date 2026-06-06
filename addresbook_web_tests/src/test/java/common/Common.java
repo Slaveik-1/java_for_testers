b01ce9600cca46a049355432e86fa1d751e8f8d6
pupsovich <slawa2013@gmail.com>
2026-06-06 20:58:29 +0400

    Лекция 7.3
    Функции-трансформеры

--- addresbook_web_tests/src/test/java/common/Common.java
+++ addresbook_web_tests/src/test/java/common/Common.java
@@ -1,19 +1,21 @@
 package common;
 
 import java.util.Random;
+import java.util.function.Supplier;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Common {
 
 
     public static String randomString(int n){
         var r = new Random();
-        var result = "";
-        for (int i =0;i<n;i++){
-            result = result+(char)('a'+r.nextInt(26/*33*/));
-        }
-//        if (n<5){
-//            result = result+ '\'';
-//        }
-        return result;
+        Supplier<Integer> randomNumbers = ()-> r.nextInt(26);
+        return Stream.generate(randomNumbers)
+                .limit(n)
+                .map(i->'a'+i)
+                .map(Character::toString)
+                .collect(Collectors.joining());
     }
 }
