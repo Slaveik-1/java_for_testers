7471b2d35b020c74ab0de72a133110837814363e
pupsovich <slawa2013@gmail.com>
2026-06-13 01:11:25 +0400

    Лекция 8.2.

---
+++ mantis_tests/src/test/java/ru/stqa/mantis/manager/ApplicationManager.java
@@ -0,0 +1,51 @@
+package ru.stqa.mantis.manager;
+
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.Properties;
+
+public class ApplicationManager {
+
+    private WebDriver driver;
+    private String browser;
+    private Properties properties;
+    private SessionHelper session;
+
+
+    //init...(выбор браузера/остановка сессии/стартовая страница)
+    public void init(String browser, Properties properties) {
+        this.browser = browser;
+        this.properties = properties;
+
+        }
+
+
+    public WebDriver driver(){
+        if (driver==null){
+            if ("firefox".equals(browser)){
+                driver = new FirefoxDriver();
+            } else if ("chrome".equals(browser)) {
+                driver = new ChromeDriver();
+            }
+            else {
+                throw new IllegalArgumentException(String.format("Unknown browser %s", browser));
+            }
+            Runtime.getRuntime().addShutdownHook(new Thread(driver::quit));
+            driver.get(properties.getProperty("web.baseURL"));
+            driver.manage().window().setSize(new Dimension(1936, 1048));
+            //  session().login(properties.getProperty("web.userName"), properties.getProperty("web.password"));
+        }
+        return driver;
+    }
+
+    public SessionHelper session(){
+        if (session==null){
+            session=new SessionHelper(this);
+        }
+        return session;
+    }
+
+}
